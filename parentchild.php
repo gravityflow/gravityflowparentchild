ce208b786f135cc698f8e550fdf7cc91d023a713
Steve Henty <stevehenty@gmail.com>
2016-03-18 17:03:35 +0100

    first commit

---
+++ parentchild.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Gravity Flow Parent-Child Forms Extension
+Plugin URI: http://gravityflow.io
+Description: Gravity Flow Parent-Child Forms Extension for Gravity Flow.
+Version: 1.0.0.3
+Author: Steve Henty
+Author URI: http://www.stevenhenty.com
+License: GPL-3.0+
+
+------------------------------------------------------------------------
+Copyright 2015-2016 Steven Henty
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+define( 'GRAVITY_FLOW_PARENT_CHILD_VERSION', '1.0.0.3' );
+define( 'GRAVITY_FLOW_PARENT_CHILD_EDD_ITEM_NAME', 'Parent-Child Forms' );
+
+add_action( 'gravityflow_loaded', array( 'Gravity_Flow_Parent_Child_Bootstrap', 'load' ), 1 );
+
+class Gravity_Flow_Parent_Child_Bootstrap {
+
+	public static function load() {
+
+		require_once( 'class-parent-child.php' );
+
+		gravity_flow_parent_child();
+	}
+}
+
+function gravity_flow_parent_child() {
+	if ( class_exists( 'Gravity_Flow_Parent_Child' ) ) {
+		return Gravity_Flow_Parent_Child::get_instance();
+	}
+}
+
+
+add_action( 'admin_init', 'gravityflowparentchild_edd_plugin_updater', 0 );
+
+function gravityflowparentchild_edd_plugin_updater() {
+
+	if ( ! function_exists( 'gravity_flow_parent_child' ) ) {
+		return;
+	}
+
+	$gravity_flow_parent_child = gravity_flow_parent_child();
+	if ( $gravity_flow_parent_child ) {
+		$settings = $gravity_flow_parent_child->get_app_settings();
+
+		$license_key = trim( rgar( $settings, 'license_key' ) );
+
+		$edd_updater = new Gravity_Flow_EDD_SL_Plugin_Updater( GRAVITY_FLOW_EDD_STORE_URL, __FILE__, array(
+			'version'   => GRAVITY_FLOW_PARENT_CHILD_VERSION,
+			'license'   => $license_key,
+			'item_name' => GRAVITY_FLOW_PARENT_CHILD_EDD_ITEM_NAME,
+			'author'    => 'Steven Henty',
+		) );
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Gravity Flow Parent-Child Extension ===
+Contributors: stevehenty
+Tags: gravity forms, approvals, workflow
+Requires at least: 4.0
+Tested up to: 4.4
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Create parent-child relationships between forms in Gravity Flow.
+
+== Description ==
+
+The Gravity Flow Parent-Child Extension is an extension for Gravity Flow.
+
+Gravity Flow is a commercial Add-On for [Gravity Forms](https://gravityflow.io/gravityforms)
+
+Facebook: [Steven Henty](https://www.facebook.com/gravityflow.io)
+
+= Requirements =
+
+1. [Purchase and install Gravity Forms](https://gravityflow.io/gravityforms)
+1. [Purchase and install Gravity Flow](https://gravityflow.io)
+1. Wordpress 4.3+
+1. Gravity Forms 1.9.4+
+1. Gravity Flow 1.1.0.4+
+
+
+= Support =
+If you find any that needs fixing, or if you have any ideas for improvements, please get in touch:
+https://gravityflow.io/contact/
+
+
+== Installation ==
+
+1.  Download the zipped file.
+1.  Extract and upload the contents of the folder to /wp-contents/plugins/ folder
+1.  Go to the Plugin management page of WordPress admin section and enable the 'Gravity Flow Parent-Child Extension' plugin.
+
+== Frequently Asked Questions ==
+
+= Which license of Gravity Flow do I need? =
+The Gravity Flow Parent-Child Extension will work with any license of [Gravity Flow](https://gravityflow.io).
+
+
+== ChangeLog ==
+
+= 1.0.0.2 =
+- Added the gravityflowparentchild_add_child_entry_link filter.
+    Example:
+    add_filter( 'gravityflowparentchild_add_child_entry_link', 'sh_filter_gravityflowparentchild_add_child_entry_link', 10, 5 );
+    function sh_filter_gravityflowparentchild_new_entry_link( $form_link, $form_url, $parent_form_id, $child_form_id, $parent_entry_id ) {
+        $form_url = site_url() . '/new-child-entry-form/?workflow_parent_entry_id=' . $parent_entry_id;
+        $form_url = esc_url( $form_url );
+        $form_link = sprintf( '<a href="%s">New Evaluation</a>', $form_url );
+        return $form_link;
+    }
+
+= 1.0.0.1 =
+- Added the gravityflowparentchild_view_all capability to control who can see the child entries.
+
+= 1.0.0 =
+All new!
